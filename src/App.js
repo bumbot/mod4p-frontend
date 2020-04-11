178dc34aaae744027ffbc737fddc2474f1cafc18
willmharris <willmharris0@gmail.com>
2020-04-11 15:02:26 -0400

    Added basic Spotify player functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
+import Player from "./Components/Player.js"
+import Search from "./Components/Search.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const authEndpoint = 'https://accounts.spotify.com/authorize'
+// The unique id for our app registered with Spotify 
+const clientId = "8836102077dc476b87b683e9fbcd411a"
+// Where to return after the user has logged into their Spotify account
+const redirectUri = "http://localhost:3000"
+// The scope is the set of permissions the user gives our app
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-modify-playback-state"
+]
+// This is the full url our app will call
+const loginLink = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      // After a sucessful login to Spotify, the Spotify API will return to our app page with our access token in the site's url
+      // The substring method allows us to ignore the #access_token= in the url and just gets the token itself
+      // Every fetch call to the Spotify API will need this token as part of its header 
+      // Tokens expire after 1 hour 
+      accessToken: window.location.hash.substring(14)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a href={loginLink}>Login to Spotify</a>
+          <Search token={this.state.accessToken} />
+          <Player token={this.state.accessToken}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Search.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react"
+
+class Search extends Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            formInput: "",
+            // Each song is put into this array, and will be mapped over to create song cards 
+            searchResults: []
+        }
+    }
+
+    updateFormInput = (event) => {
+        this.setState({formInput: event.target.value})
+    }
+
+    submitSearch = (event) => {
+        event.preventDefault()
+        fetch(`https://api.spotify.com/v1/search?q=name:${this.state.formInput}&type=track`, {
+            headers: {
+                "Authorization": "Bearer " + this.props.token
+            }
+        }).then(
+            resp => resp.json()
+        ).then(
+            data => this.setState({searchResults: data.tracks.items})
+        )
+    }
+
+    // In order get immediate song playback when the user clicks the song card, you have to get a little sneaky. 
+    // First you have to grab the song uri (which is set as its div id when it's created)
+    // Then you have to add that song to the Play Queue with a POST request 
+    // Finally you have to command the player to go to the next song (which is the song we just put in the queue)
+    // This allows immediate playback of any song clicked on the page
+    
+    next = () => {
+        fetch("https://api.spotify.com/v1/me/player/next", {
+          method: "POST",
+          headers: {
+            "Authorization": "Bearer " + this.props.token
+          }
+        })
+    }
+
+    playSong = (event) => {
+        let token = this.props.token
+        fetch(`https://api.spotify.com/v1/me/player/queue?uri=${event.target.parentElement.id}`, {
+        method: "POST", 
+        headers: {
+                "Authorization": "Bearer " + this.props.token
+            }
+        }).then(this.next)
+    }
+    
+    render() {
+        return(
+            <form>
+                <input type="text" onChange={this.updateFormInput}></input>
+                <input type="submit" onClick={this.submitSearch}></input>
+                {this.state.searchResults.map(result => {
+                    return( 
+                        <div onClick={this.playSong} id={result.uri}>
+                            <h3>{result.name}</h3>
+                            <img src={result.album.images[0].url} width={250} height={250} />
+                        </div>
+                    )
+                })}
+            </form>
+        )
+    }
+}
+
+export default Search
